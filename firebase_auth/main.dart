481564568115f211649e69e334b31489cbba0a37
sagarkumar2804 <45657401+sagarkumar2804@users.noreply.github.com>
2021-05-09 14:35:18 +0530

    Add files via upload

---
+++ firebase_auth/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lit_firebase_auth/lit_firebase_auth.dart';
+import 'package:lit_firebase_ui_demo/config/palette.dart';
+import 'package:lit_firebase_ui_demo/screens/splash.dart';
+
+//import 'screens/home.dart';
+//import 'screens/auth/auth.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LitAuthInit(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: GoogleFonts.muliTextTheme(),
+          accentColor: Palette.darkOrange,
+          appBarTheme: const AppBarTheme(
+            brightness: Brightness.dark,
+            color: Palette.darkBlue,
+          ),
+        ),
+
+        // home: const LitAuthState(
+        //   authenticated: Home(),
+        //   unauthenticated: Unauthenticated(),
+        // ),
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
